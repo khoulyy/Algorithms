15760c2ae6f2807060b194d6028fafb8115bbce3
khoulyy <abdoelkhouly555@gmail.com>
2021-12-15 22:28:01 +0200

    Add linear Search with strings Algorithm

---
+++ linearSearch_Strings.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main()
+{
+    string names[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
+
+    for (int i = 0; i < 7; i++)
+    {
+        // strcmp() function to compare two strings
+        if (strcmp(names[i], "Ron") == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
