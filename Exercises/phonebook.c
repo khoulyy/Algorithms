36b9a3f55ee997a5b7b2edc96476f31e5fb425c7
khoulyy <abdoelkhouly555@gmail.com>
2021-12-16 14:04:12 +0200

    Add Exercises and phonebook example

---
+++ Exercises/phonebook.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+// To make your own data type
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
+int main()
+{
+    person people[2];
+    
+    people[0].name = "Brian";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-949-468-2750";
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(people[i].name, "David") == 0)
+        {
+            printf("Found %s\n", people[i].number);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
